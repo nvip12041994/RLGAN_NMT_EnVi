1024115f9baf5f45743bc6ccbcd3d9c53e0bd23d
nvip <nvip12041994@gmail.com>
2021-08-04 21:42:08 +0700

    fix

--- joint_score.sh
+++ joint_score.sh
@@ -6,7 +6,7 @@ for file in checkpoints/joint/*; do
     folder_name=${name/.pt/ }
     mkdir -p joint/$folder_name
     python3 joint_generate.py --model_file checkpoints/joint/$(basename "$file") --data new-data-bin/iwslt15.tokenized.en-vi/ --src_lang en --trg_lang vi --batch-size 16 --gpuid 0
-    mv real.txt joint/$folder_name
-    mv predictions.txt joint/$folder_name
-    perl scripts/multi-bleu.perl joint/$folder_name/real.txt < joint/$folder_name/predictions.txt
+    perl scripts/multi-bleu.perl real.txt < predictions.txt
+    mv -v real.txt joint/$folder_name/
+    mv -v predictions.txt joint/$folder_name/
 done
