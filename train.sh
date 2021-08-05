0154092e0c48c6b473bef555654569fce2486d31
nvip <nvip12041994@gmail.com>
2021-08-05 23:30:26 +0700

    fix train.sh

--- train.sh
+++ train.sh
@@ -2,6 +2,7 @@
 
 # --criterion RL \
 # --sample-beam 5 \
+# --bpe subword_nmt \
 
 ARCH=transformer
 export CUDA_VISIBLE_DEVICES=0
@@ -14,10 +15,9 @@ fairseq-train ${DATA_PATH} \
 --user-dir /home/s1910443/experiment/change_seq/RLGAN_NMT_EnVi \
 --arch $ARCH \
 --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.1 \
---dropout 0.2 \
---optimizer adam --lr 0.005 --lr-shrink 0.5 \
---max-tokens 12000 \
+--dropout 0.3 --weight-decay 0.0001 \
+--optimizer adam --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
+--max-tokens 4096 \
 --save-dir $MODEL_PATH \
 --seed 2048 \
---bpe subword_nmt \
 --restore-file $MODEL_PATH/checkpoint_best.pt
