535ef0a2921d56f2e0edd9d1438b6130c09ff6da
nvip <nvip12041994@gmail.com>
2021-08-05 22:08:41 +0700

    test gru_transformer

--- fairseq-pre.sh
+++ fairseq-pre.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-TEXT=./data
+TEXT=./pure_data
 fairseq-preprocess  --source-lang en --target-lang vi \
                     --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test \
+                    --joined-dictionary \
                     --destdir new-data-bin/iwslt15.tokenized.en-vi 
