e77bb8a1b80d87351dc460341206e2c6a6d3634b
nvip <nvip12041994@gmail.com>
2021-08-05 22:02:11 +0700

    test gru_transformer

--- train.sh
+++ train.sh
@@ -1,1 +1,9 @@
-python joint_train.py --data new-data-bin/iwslt15.tokenized.en-vi  --src_lang en --trg_lang vi --learning_rate 1e-3 --joint-batch-size 64 --gpuid 0 --clip-norm 1.0 --epochs 10
+#!/usr/bin/bash
+data=/home/nvip/data/RLGAN_NMT_EnVi/new-data-bin/iwslt15.tokenized.en-vi/
+fairseq-train ${data} --task translation --user-dir /home/s1910443/experiment/change_seq/RLGAN_NMT_EnVi \
+    --arch gru_transformer --encoder-layers 2 --decoder-layers 2 --dropout 0.3 --share-all-embeddings \
+    --optimizer adam --adam-betas '(0.9, 0.98)' \
+    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
+    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+    --log-format 'simple' --log-interval 100 --save-dir "checkpoints/${VOCAB}" \
+    --batch-size 100 --max-update 100000 --update-freq 2
