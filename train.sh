54ffffd6f03fbcd483329f65d7c5f7f45daf1536
nvip <nvip12041994@gmail.com>
2021-08-05 23:26:03 +0700

    fix train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# --criterion RL \
+# --sample-beam 5 \
+
 ARCH=transformer
 export CUDA_VISIBLE_DEVICES=0
 
@@ -11,8 +14,6 @@ fairseq-train ${DATA_PATH} \
 --user-dir /home/s1910443/experiment/change_seq/RLGAN_NMT_EnVi \
 --arch $ARCH \
 --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.1 \
-# --criterion RL \
-# --sample-beam 5 \
 --dropout 0.2 \
 --optimizer adam --lr 0.005 --lr-shrink 0.5 \
 --max-tokens 12000 \
