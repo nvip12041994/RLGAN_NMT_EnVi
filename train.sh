b6c29e322a07cd58160226153e053572c9e9610d
nvip <nvip12041994@gmail.com>
2021-08-05 23:17:09 +0700

    fix train

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 data=./new-data-bin/iwslt15.tokenized.en-vi/
 fairseq-train ${data} --task translation --user-dir /home/s1910443/experiment/change_seq/RLGAN_NMT_EnVi \
-    --arch transformer_pointer_generator --encoder-layers 2 --decoder-layers 2 --dropout 0.3 --share-all-embeddings \
+    --arch transformer --share-all-embeddings \
     --optimizer adam --adam-betas '(0.9, 0.98)' \
     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --log-format 'simple' --log-interval 100 --save-dir "checkpoints/${VOCAB}" \
-    --batch-size 100 --max-update 100000 --update-freq 2
+    --batch-size 64 --max-update 100000 --update-freq 2
