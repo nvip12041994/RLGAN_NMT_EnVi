aea787693fe3540200ca848476aed661846bcb05
nvip <nvip12041994@gmail.com>
2021-08-05 23:20:18 +0700

    fix train

--- train.sh
+++ train.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/bash
-data=./new-data-bin/iwslt15.tokenized.en-vi/
-fairseq-train ${data} --task translation --user-dir /home/s1910443/experiment/change_seq/RLGAN_NMT_EnVi \
-    --arch transformer --share-all-embeddings \
-    --optimizer adam --adam-betas '(0.9, 0.98)' \
-    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
-    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
-    --log-format 'simple' --log-interval 100 --save-dir "checkpoints/${VOCAB}" \
-    --batch-size 64 --max-update 100000 --update-freq 2
+
+ARCH=transformer
+export CUDA_VISIBLE_DEVICES=0
+
+DATA_PATH=./new-data-bin/iwslt15.tokenized.en-vi/
+MODEL_PATH=./model/
+
+
+fairseq-train ${DATA_PATH} \
+--user-dir /home/s1910443/experiment/change_seq/RLGAN_NMT_EnVi \
+--arch $ARCH \
+--optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.1 \
+# --criterion RL \
+# --sample-beam 5 \
+--encoder-dropout 0.2 --decoder-dropout 0.2 \
+--optimizer adam --lr 0.005 --lr-shrink 0.5 \
+--max-tokens 12000 \
+--save-dir $MODEL_PATH \
+--seed 2048 \
+--bpe subword_nmt \
+--restore-file $MODEL_PATH/checkpoint_best.pt
