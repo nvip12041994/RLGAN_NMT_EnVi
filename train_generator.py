699abfb1db3e912ea5fb22df6f64fd0297d410d3
nvip <nvip12041994@gmail.com>
2021-09-10 10:20:07 +0700

    test

--- train_generator.py
+++ train_generator.py
@@ -474,7 +474,7 @@ def cli_main(
                         '--lr', '0.0005', '--clip-norm', '0.0',   
                         '--label-smoothing', '0.1', '--seed', '2048',
                         '--max-tokens', '4096',
-                        '--max-epoch', '17',
+                        '--max-epoch', '18',
                         '--lr-scheduler', 'inverse_sqrt',
                         '--weight-decay', '0.0',   
                         '--criterion', 'label_smoothed_cross_entropy',
