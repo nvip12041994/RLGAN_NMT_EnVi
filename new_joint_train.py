f927144cbd1c50ce7020f2ddfb35ffce67c180d0
nvip <nvip12041994@gmail.com>
2021-09-10 08:38:02 +0700

    test

--- new_joint_train.py
+++ new_joint_train.py
@@ -300,11 +300,13 @@ def train(
             "train_step-%d" % i
         ):
             log_output = trainer.train_step(samples)
-            print("----------------------------------------------------------------")
+            print("--------------------START DEBUG---------------------------------")
+            print("SAMPLE")
             print(samples)
             print("----------------------------------------------------------------")
+            print("LOG_OUTPUT")
             print(log_output)
-            print("----------------------------------------------------------------")
+            print("--------------------END DEBUG----------------------------------")
         if log_output is not None:  # not OOM, overflow, ...
             # log mid-epoch stats
             num_updates = trainer.get_num_updates()
@@ -506,7 +508,7 @@ def cli_main(
                         '--lr', '0.0005', '--clip-norm', '0.0',   
                         '--label-smoothing', '0.1', '--seed', '2048',
                         '--max-tokens', '4096',
-                        '--batch-size', '128',
+                        '--batch-size', '256',
                         '--max-epoch', '16',
                         '--lr-scheduler', 'inverse_sqrt',
                         '--weight-decay', '0.0',   
