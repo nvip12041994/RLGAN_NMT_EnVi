0d0f7b228415670bcd1a4d33a0f15aed95b0d1f4
nvip <nvip12041994@gmail.com>
2021-09-12 18:56:38 +0700

    test

--- modify_trainer.py
+++ modify_trainer.py
@@ -867,6 +867,13 @@ class Trainer(object):
             )
 
         metrics.log_stop_time("train_wall")
+        print("--------------------START DEBUG---------------------------------")
+        # print("SAMPLE")
+        print(sample_size)
+        # print("----------------------------------------------------------------")
+        # print("LOG_OUTPUT")
+        # print(logging_output)
+        print("--------------------END DEBUG----------------------------------")
         return logging_output
 
     @metrics.aggregate("valid")
